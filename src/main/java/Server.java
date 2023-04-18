bd83978d7ce4485f3e03a5632665532f7bd3527f
刘佳宁 <8782095+giteeljn@user.noreply.gitee.com>
2023-04-18 20:17:20 +0800

    提交到temp分支

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,2 +1,51 @@
-package PACKAGE_NAME;public class Server {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server implements Runnable {
+    // 不知道需要socket
+    private Socket socket;
+
+    public Server(Socket socket) {
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+        try(
+            BufferedReader bf = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter pw = new PrintWriter(socket.getOutputStream())
+        ) {
+            String info = bf.readLine();
+            if (info == null)
+                return;
+            if (info.equals("CONSUME")) {
+                String msg = SimpleMQ.consume();
+                if (msg == null)
+                    pw.println("获取消息失败: 消息队列已空");
+                else
+                    pw.println(msg);
+            } else {
+                SimpleMQ.produce(info);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(9999);
+        System.out.println("服务器启动，等待连接！");
+        System.out.println("===========================================");
+        // 为了保证服务器可以与多个客户端建立连接，即服务器一致处于等待连接的状态
+        while (true) {
+            Socket socket = serverSocket.accept();
+            Server server = new Server(socket);
+            // 使用不同的线程处理请求，提高响应速度
+            new Thread(server).start();
+        }
+    }
 }
